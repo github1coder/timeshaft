bd38ce74cf697bfcea5e19a384f166da53438b60
周子颖 <19373132@buaa.edu.cn>
2022-04-09 22:51:45 +0800

    init after_end

---
+++ after_end/src/main/java/com/timeshaft/after_end/AfterEndApplication.java
@@ -0,0 +1,13 @@
+package com.timeshaft.after_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterEndApplication.class, args);
+    }
+
+}
