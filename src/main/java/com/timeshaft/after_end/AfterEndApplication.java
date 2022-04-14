68ca58ff8219428b12dd3f46c2dfb9d43603497f
周子颖 <19373132@buaa.edu.cn>
2022-04-15 00:31:20 +0800

    test

--- src/main/java/com/timeshaft/after_end/AfterEndApplication.java
+++ src/main/java/com/timeshaft/after_end/AfterEndApplication.java
@@ -3,13 +3,25 @@ package com.timeshaft.after_end;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @MapperScan
+@Controller
+@CrossOrigin
 public class AfterEndApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AfterEndApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Hello";
     }
 
 }
--- src/main/java/com/timeshaft/after_end/controller/LoginController.java
+++ src/main/java/com/timeshaft/after_end/controller/LoginController.java
@@ -5,33 +5,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
 
 @Controller
 public class LoginController {
-    @Autowired
-    UserService userService;
-
-    @GetMapping("")
-    public Object login(@RequestParam(value = "email") String email,
-                        @RequestParam(value = "password") String password,
-                        Model model,
-                        HttpSession session
-    ) {
-//        if (userService.login()) {
+//    @Autowired
+//    UserService userService;
 //
-//        }
+//    @GetMapping("")
+//    public Object login(@RequestParam(value = "email") String email,
+//                        @RequestParam(value = "password") String password,
+//                        Model model,
+//                        HttpSession session
+//    ) {
+////        if (userService.login()) {
+////
+////        }
+////        return "";
+//    }
+
+//    @GetMapping("")
+//    public Object logout(Model model,
+//                         HttpSession session
+//    ) {
+//
 //        return "";
-    }
-
-    @GetMapping("")
-    public Object logout(Model model,
-                         HttpSession session
-    ) {
-
-        return "";
-    }
+//    }
 }
