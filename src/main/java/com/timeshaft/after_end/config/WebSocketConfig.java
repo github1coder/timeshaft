662bf63224f73bc40b5353e49ae19c3d7c0d03ce
20060109 <dxt1601@buaa.edu.cn>
2022-04-17 14:34:54 +0800

    配置websocket相关内容

---
+++ src/main/java/com/timeshaft/after_end/config/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package com.timeshaft.after_end.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * feishu
+ * Websocket相关配置
+ *
+ * @author : dxt
+ * @since : 2022-04-17 13:53
+ **/
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    /**
+     * 设置stomp端点, 即允许客户端通过此端点(/websocket)进行连接, withSockJS允许不支持websocket的浏览器使用此服务
+     *
+     * @param stompEndpointRegistry stomp端点注册器
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/websocket").withSockJS();
+    }
+
+    /**
+     * 配置client路径, 其中/user与/group是client订阅的路径前缀, /app是client发送至server所使用的路径前缀
+     *
+     * @param messageBrokerRegistry 消息路径配置器
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
+        messageBrokerRegistry.enableSimpleBroker("/user", "/group");
+        messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
+    }
+
+
+}
---
+++ src/main/java/com/timeshaft/after_end/controller/MessageController.java
@@ -0,0 +1,46 @@
+package com.timeshaft.after_end.controller;
+
+import com.timeshaft.after_end.entity.GroupMessage;
+import com.timeshaft.after_end.entity.PersonalMessage;
+import com.timeshaft.after_end.service.impl.GroupMessageServiceImpl;
+import com.timeshaft.after_end.service.impl.PersonalMessageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+/**
+ * feishu
+ * 处理客户端与服务器间信息传递
+ *
+ * @author : dxt
+ * @since : 2022-04-17 13:43
+ **/
+@Controller
+public class MessageController {
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+    @Autowired
+    private PersonalMessageServiceImpl personalMessageService;
+    @Autowired
+    private GroupMessageServiceImpl groupMessageService;
+
+    /**
+     * 接收客户端发送的私信类型消息，将其存入数据库，并发送至指定的用户路径
+     *
+     * @param personalMessage 前端传来的私信
+     */
+    public void receivePersonalMessage(@Payload PersonalMessage personalMessage) {
+
+    }
+
+    /**
+     * 接收客户端发送的群聊类型消息，将其存入数据库，并发送至指定的群组路径
+     *
+     * @param groupMessage 前端传来的群组消息
+     */
+    public void receiveGroupMessage(@Payload GroupMessage groupMessage) {
+
+    }
+}
+
