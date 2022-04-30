5541e7f58d1d179ab2fecbaae9ff08b1dcf55183
20060109 <dxt1601@buaa.edu.cn>
2022-04-30 22:44:02 +0800

    实现消息列表与订阅路径接口

--- src/test/java/com/timeshaft/after_end/AfterEndApplicationTests.java
+++ src/test/java/com/timeshaft/after_end/AfterEndApplicationTests.java
@@ -6,10 +6,12 @@ import com.timeshaft.after_end.service.ResponseService;
 import com.timeshaft.after_end.service.addressList.FriendOp;
 import com.timeshaft.after_end.service.userop.UserOp;
 import org.junit.jupiter.api.Test;
+import org.omg.CORBA.OBJECT_NOT_EXIST;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,7 +27,11 @@ class AfterEndApplicationTests {
 
     @Test
     void contextLoads() {
-
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("sourceId", 15);
+        ResponseService responseService = chatController.getMessagesList(map);
+        Object chatName = ((ArrayList<HashMap<String, Object>>) responseService.getData()).get(0);
+        System.out.println(((HashMap<String, Object>) chatName).get("chatName"));
     }
 
 }
