93ed56291cb2e036b0319f00e7dc291189ed935c
20060109 <dxt1601@buaa.edu.cn>
2022-04-30 14:34:59 +0800

    websocket跨域问题

--- src/main/java/com/timeshaft/after_end/config/WebSocketConfig.java
+++ src/main/java/com/timeshaft/after_end/config/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-        stompEndpointRegistry.addEndpoint("/websocket").withSockJS();
+        stompEndpointRegistry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
     }
 
     /**
