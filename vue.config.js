48fe496f78a5bd5becd3fdde6c705182196d1170
周子颖 <19373132@buaa.edu.cn>
2022-05-02 20:28:06 +0800

    修改端口

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,10 @@ module.exports = {
         extract: {
             filename: "style.css"
         }
+    },
+    devServer: {
+        port: 443 // 此处修改你想要的端口号
     }
-
     // devServer: {
     //     '/api': {
     //         traget: 'http://localhost:8081',
