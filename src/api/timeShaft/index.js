7c8422df1fc1806144712071a2795f3a89471ae9
AaronHuang <398838870@qq.com>
2022-05-08 16:29:46 +0800

    修改logo

--- src/api/timeShaft/index.js
+++ src/api/timeShaft/index.js
@@ -1,1 +1,9 @@
 import request from "../request"
+
+export function getTimeLine(param) {
+    return request({
+        url: '/message/getSubscribeUrlList',
+        method: 'post',
+        data: param
+    })
+}
