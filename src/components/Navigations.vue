0019ba474addfc287acf98ca2ad6d610a75bbef9
颜月 <19373400@buaa.edu.cn>
2022-05-09 10:29:41 +0800

    info

--- src/components/Navigations.vue
+++ src/components/Navigations.vue
@@ -124,6 +124,7 @@ export default {
         this.$store.commit("setLogin", false)
         this.$store.commit("setInfoId", -1)
         this.$store.commit("setInfoName", null)
+        this.$store.commit("setInfoNick", null)
         this.$store.commit("setInfoPhoto", null)
         this.$store.commit("setMaster", -1)
         this.$store.commit("changeSiderState", 0)
