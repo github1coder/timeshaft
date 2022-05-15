7b1479ec3f7c34ef99ec94b8bb8461dbca6ca853
19373131 <2230865321@qq.com>
2022-05-15 10:16:13 +0800

    111

--- src/main/java/com/timeshaft/after_end/service/userop/MyPasswordEncoder.java
+++ src/main/java/com/timeshaft/after_end/service/userop/MyPasswordEncoder.java
@@ -1,15 +1,25 @@
 package com.timeshaft.after_end.service.userop;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.DigestUtils;
 
 @Component
 public class MyPasswordEncoder {
+//    public String encode(String rawPassword) {
+//        return DigestUtils.md5DigestAsHex(rawPassword.getBytes());
+//    }
+//
+//    public boolean matches(String rawPassword, String encodedPassword) {
+//        return encodedPassword.equals(DigestUtils.md5DigestAsHex(rawPassword.getBytes()));
+//    }
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
     public String encode(String rawPassword) {
-        return DigestUtils.md5DigestAsHex(rawPassword.getBytes());
+        return encoder.encode(rawPassword);
     }
 
     public boolean matches(String rawPassword, String encodedPassword) {
-        return encodedPassword.equals(DigestUtils.md5DigestAsHex(rawPassword.getBytes()));
+        return encoder.matches(rawPassword, encodedPassword);
     }
+
 }
--- src/test/java/com/timeshaft/after_end/AfterEndApplicationTests.java
+++ src/test/java/com/timeshaft/after_end/AfterEndApplicationTests.java
@@ -9,6 +9,7 @@ import com.timeshaft.after_end.service.ResponseService;
 import com.timeshaft.after_end.service.UserService;
 import com.timeshaft.after_end.service.addressList.FriendOp;
 import com.timeshaft.after_end.service.userop.MailService;
+import com.timeshaft.after_end.service.userop.MyPasswordEncoder;
 import com.timeshaft.after_end.service.userop.UserOp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 @SpringBootTest
 @Service
@@ -36,7 +38,36 @@ class AfterEndApplicationTests {
     private MailService mailService;
 
     @Test
-    void contextLoads() {
-        System.out.println(3*0.1/2);
+    void contextLoads() throws Exception {
+//        for(int i = 30; i < 162; i++) {
+//            User user = userService.queryById(i);
+//            MyPasswordEncoder encoder = new MyPasswordEncoder();
+//            String str = randomPassword(9);
+//            user.setPassword(encoder.encode(str));
+//            userService.update(user);
+//        }
+        userOp.login("123456@qq.com", "123456");
     }
+
+    private static final int MIN_NUMBER = 33;
+    private static final int MAX_NUMBER = 126;
+    private static final int BOUND = MAX_NUMBER - MIN_NUMBER + 1;
+
+    /**
+     * 随机生成指定位数的密码
+     *
+     * char[33,126]，可表示数字、大小写字母、特殊字符
+     *
+     * @param length 密码长度
+     */
+    public static String randomPassword(int length) {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char value = (char) (random.nextInt(BOUND) + MIN_NUMBER);
+            builder.append(value);
+        }
+        return builder.toString();
+    }
+
 }
