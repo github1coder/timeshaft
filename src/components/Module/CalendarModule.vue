83a0ba407e517497c2bc43ddce22c19a3aa0f500
徐家乐 <19373224@buaa.edu.cn>
2022-05-17 20:01:38 +0800

    日历获取时间轴数据基本实现

--- src/components/Module/CalendarModule.vue
+++ src/components/Module/CalendarModule.vue
@@ -205,33 +205,34 @@ export default {
     },
 
     updateRange ({ start, end }) {
-      const events = []
+      // const events = []
 
       const min = new Date(`${start.date}T00:00:00`)
       const max = new Date(`${end.date}T23:59:59`)
-      const days = (max.getTime() - min.getTime()) / 86400000
-      const eventCount = this.rnd(days, days + 20)
+      // const days = (max.getTime() - min.getTime()) / 86400000
+      // const eventCount = this.rnd(days, days + 20)
       console.log("min:" + this.timestampToTime(min.getTime()))
+      console.log("max:" + max)
       console.log("max:" + this.timestampToTime(max.getTime()))
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-        const second = new Date(first.getTime() + secondTimestamp)
-
-        // console.log(this.timestampToTime(first))
-        // console.log(this.timestampToTime(second))
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: this.timestampToTime(first),
-          end: this.timestampToTime(second),
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-        })
-      }
-      console.log("====")
-      console.log(events)
+      // for (let i = 0; i < eventCount; i++) {
+      //   const allDay = this.rnd(0, 3) === 0
+      //   const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+      //   const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+      //   const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+      //   const second = new Date(first.getTime() + secondTimestamp)
+      //
+      //   // console.log(this.timestampToTime(first))
+      //   // console.log(this.timestampToTime(second))
+      //   events.push({
+      //     name: this.names[this.rnd(0, this.names.length - 1)],
+      //     start: this.timestampToTime(first),
+      //     end: this.timestampToTime(second),
+      //     color: this.colors[this.rnd(0, this.colors.length - 1)],
+      //     timed: !allDay,
+      //   })
+      // }
+      // console.log("====")
+      // console.log(events)
       getTimeShaftData({
         start: this.timestampToTime(min.getTime()),
         end: this.timestampToTime(max.getTime())
@@ -239,8 +240,6 @@ export default {
         console.log("----")
         console.log(res)
         for(let i in res) {
-          res[i].start = this.timestampToTime(res[i].start)
-          res[i].end = this.timestampToTime(res[i].end)
           res[i].color = this.colors[this.rnd(0, this.colors.length - 1)]
         }
         this.events = res
