c7ffcad0b59c570cd0fa380e3f2bb7755792f855
周子颖 <19373132@buaa.edu.cn>
2022-05-20 14:50:33 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,15 @@ module.exports = {
     },
     devServer: {
         port: 8081 // 此处修改你想要的端口号
-    }
+    },
+
+    configureWebpack: (config) => {
+      config.mode = 'production';
+      config["performance"] = {//打包文件大小配置
+        "maxEntrypointSize": 10000000,
+        "maxAssetSize": 30000000
+      }
+  }
     // devServer: {
     //     '/api': {
     //         traget: 'http://localhost:8081',
