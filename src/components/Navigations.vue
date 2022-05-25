909d6006396641d557694cc5da18b01e5209b1a7
AaronHuang <398838870@qq.com>
2022-05-25 22:03:28 +0800

    UI更新4

--- src/components/Navigations.vue
+++ src/components/Navigations.vue
@@ -45,7 +45,7 @@
                     text
                     @click="editAccount"
                   >
-                    Edit Account
+                    编辑信息
                   </v-btn>
                   <v-divider class="my-3"></v-divider>
                   <v-btn
@@ -54,7 +54,7 @@
                     text
                     @click="disconnect"
                   >
-                    Disconnect
+                    退出登录
                   </v-btn>
                 </div>
               </v-list-item-content>
@@ -81,13 +81,11 @@
                   :value="$store.state.unreadNum !== 0 && i === 0"
                   :content="$store.state.unreadNum"
                 >
-                <v-icon v-text="item.icon"></v-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                  <v-list-item-title v-text="item.text" style="font-size: 5px"></v-list-item-title>
               </v-badge>
             </v-list-item-icon>
 
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -105,16 +103,16 @@ export default {
       model: 0,
       navs: [{
         icon: 'mdi-message-text',
-        text: '',
+        text: '消息栏',
       }, {
         icon: 'mdi-account-box-multiple-outline',
-        text: '',
+        text: '通讯录',
       }, {
         icon: 'mdi-file-document-multiple-outline',
-        text: '',
+        text: '文件夹',
       }, {
         icon: 'mdi-calendar-check',
-        text: '',
+        text: '日历表',
       },],
     }
   },
